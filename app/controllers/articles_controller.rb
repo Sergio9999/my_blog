9bc89326f00f3a4015875b027613734b42b7756d
Sergey Sobolev <Sergrpm@mail.ru>
2016-02-17 11:18:54 +0200

    Finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "Sergio9999", password: "DR260733", except: [:index, :show]
 	def index
 		@articles = Article.all
 	end
